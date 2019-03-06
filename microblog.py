78087aa8726e680b531f7d94de61f8457dfcee22
Charles Ross <chivalry@mac.com>
2019-03-05 19:11:13 -0800

    temp commit

--- microblog.py
+++ microblog.py
@@ -1,10 +1,14 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 from app import app, db
 from app.models import User, Post
 
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
+=======
+from app import app
+>>>>>>> parent of e59b5a8... Add shell context processor
 =======
 from app import app
 >>>>>>> parent of e59b5a8... Add shell context processor
