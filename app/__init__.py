10d6f43664a6114b5d39b6cf29797a6617f7245f
Charles Ross <chivalry@mac.com>
2019-03-07 12:40:12 -0800

    Git translations

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,9 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
-from config import Config
 from flask_moment import Moment
-from flask_babel import Babel
+from flask_babel import Babel, lazy_gettext as _l
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,7 +17,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-login.login_message = _l('Please log into to access this page.')
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
@@ -51,8 +51,10 @@ if not app.debug:
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
 
-from app import routes, models, errors
 
 @babel.localeselector
 def get_locale():
-    return request.accept_lanaguages.bast_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+from app import routes, models, errors
