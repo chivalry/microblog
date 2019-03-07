9b9afcac9af12448ed145c35541c60aeb6751e15
Charles Ross <chivalry@mac.com>
2019-03-07 11:46:57 -0800

    Facelift override

--- migrations/versions/e517276bb1c2_users_table.py
+++ migrations/versions/e517276bb1c2_users_table.py
@@ -1,8 +1,8 @@
 """users table
 
-Revision ID: 77894652c260
+Revision ID: e517276bb1c2
 Revises: 
-Create Date: 2019-03-05 16:55:55.033395
+Create Date: 2017-09-11 11:23:05.566844
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '77894652c260'
+revision = 'e517276bb1c2'
 down_revision = None
 branch_labels = None
 depends_on = None
--- tests.py
+++ tests.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python
 from datetime import datetime, timedelta
 import unittest
 from app import app, db
 from app.models import User, Post
+
 
 class UserModelCase(unittest.TestCase):
     def setUp(self):
@@ -57,10 +59,14 @@ class UserModelCase(unittest.TestCase):
 
         # create four posts
         now = datetime.utcnow()
-        p1 = Post(body='post from john', author=u1, timestamp=now + timedelta(seconds=1))
-        p2 = Post(body='post from susan', author=u2, timestamp=now + timedelta(seconds=4))
-        p3 = Post(body='post from mary', author=u3, timestamp=now + timedelta(seconds=3))
-        p4 = Post(body='post from david', author=u4, timestamp=now + timedelta(seconds=2))
+        p1 = Post(body="post from john", author=u1,
+                  timestamp=now + timedelta(seconds=1))
+        p2 = Post(body="post from susan", author=u2,
+                  timestamp=now + timedelta(seconds=4))
+        p3 = Post(body="post from mary", author=u3,
+                  timestamp=now + timedelta(seconds=3))
+        p4 = Post(body="post from david", author=u4,
+                  timestamp=now + timedelta(seconds=2))
         db.session.add_all([p1, p2, p3, p4])
         db.session.commit()
 
@@ -68,7 +74,7 @@ class UserModelCase(unittest.TestCase):
         u1.follow(u2)  # john follows susan
         u1.follow(u4)  # john follows david
         u2.follow(u3)  # susan follows mary
-        u3.follow(u4)  #mary follows david
+        u3.follow(u4)  # mary follows david
         db.session.commit()
 
         # check the followed posts of each user
@@ -81,6 +87,6 @@ class UserModelCase(unittest.TestCase):
         self.assertEqual(f3, [p3, p4])
         self.assertEqual(f4, [p4])
 
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
-
